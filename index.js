564570815bc6248723ffc636f71c4214a9595a7f
ion rosgrim <jrunin@gmail.com>
2020-07-14 22:59:15 +0200

    highlight words and better paginate

--- index.js
+++ index.js
@@ -3,44 +3,84 @@ const express = require('express');
 const app = express();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./db/webster.db', (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('connected to db!');
-  }
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('connected to db!');
+    }
 })
 
 app.listen(port, () => {
-  console.log(`Server 🔥 on port ${port}!`);
+    console.log(`Server 🔥 on port ${port}!`);
 })
 
 app.use(express.static('www'));
 
 app.get('/define', (request, response) => {
-  const word = request.query.word;
-  const page = parseInt(request.query.page) || 1;
-  const resultsPerPage = parseInt(request.query.resultsPerPage) || 20;
-  const query = word ? `SELECT * FROM english_dictionary WHERE word LIKE '%${word}%'` : `SELECT * FROM english_dictionary`;
-  db.all(query, (err, rows) => {
-    if (err) {
-      console.log(err);
-    } else {
-      response.send(paginatedResponse(rows, resultsPerPage, page));
-    }
-  })
+    const word = request.query.word;
+    const page = parseInt(request.query.page) || 1;
+    const resultsPerPage = parseInt(request.query.resultsPerPage) || 20;
+    const query = word ? `SELECT * FROM english_dictionary WHERE word LIKE '%${word}%'` : `SELECT * FROM english_dictionary`;
+    db.all(query, (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            response.send({
+                pagination: pagination(rows.length, page, resultsPerPage), 
+                words: paginatedResponse(rows, resultsPerPage, page)
+            });
+        }
+    })
 })
 
 function paginatedResponse(rows, resultsPerPage, page) {
-  const totalRows = rows.length;
-  const pages = Math.ceil(totalRows / resultsPerPage);
-  const currentPage = (page - 1);
-  const wordsAtPage = rows.slice(currentPage * resultsPerPage, (currentPage * resultsPerPage) + resultsPerPage);
-  return {
-    pagination: {
-      matches: totalRows,
-      currentPage: page,
-      lastPage: pages,
-    },
-    words: wordsAtPage
-  }
+    const totalRows = rows.length;
+    const pages = Math.ceil(totalRows / resultsPerPage);
+    const currentPage = (page - 1);
+    const wordsAtPage = rows.slice(currentPage * resultsPerPage, (currentPage * resultsPerPage) + resultsPerPage);
+    return  wordsAtPage;
+}
+
+function pagination ( totalItems = 0, currentPage = 1, pageSize = 5, maxPages = 5 ) {
+    const totalPages = Math.ceil(totalItems / pageSize);
+    if (currentPage < 1) {
+        currentPage = 1;
+    } else if (currentPage > totalPages) {
+        currentPage = totalPages;
+    }
+    let startPage;
+    let endPage;
+    if (totalPages <= maxPages) {
+        startPage = 1;
+        endPage = totalPages;
+    } else {
+        const maxPagesBeforeCurrentPage = Math.floor(maxPages / 2);
+        const maxPagesAfterCurrentPage = Math.ceil(maxPages / 2) - 1;
+        if (currentPage <= maxPagesBeforeCurrentPage) {
+            startPage = 1;
+            endPage = maxPages;
+        } else if (currentPage + maxPagesAfterCurrentPage >= totalPages) {
+            startPage = totalPages - maxPages + 1;
+            endPage = totalPages;
+        } else {
+            startPage = currentPage - maxPagesBeforeCurrentPage;
+            endPage = currentPage + maxPagesAfterCurrentPage;
+        }
+    }
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+    const pages = Array.from(Array(endPage + 1 - startPage).keys()).map(
+        (i) => startPage + i
+    );
+    return {
+        totalItems,
+        currentPage,
+        pageSize,
+        totalPages,
+        startPage,
+        endPage,
+        startIndex,
+        endIndex,
+        pages
+    }
 }
--- www/app/app.js
+++ www/app/app.js
@@ -1,45 +1,79 @@
 let debouncer;
 const app = new Vue({
-  el: '#app',
-  data: function () {
-    return {
-      title: 'english dictionary',
-      word: '',
-      currentPage: 1,
-      lastPage: -1,
-      words: [],
-      resultsPerPage: 20,
-    }
-  },
-  mounted: function () {
-    this.query('', this.resultsPerPage);
-  },
-  methods: {
-    query: function (word, resultsPerPage, page) {
-      axios.get(`/define?word=${word}&resultsPerPage=${resultsPerPage}&page=${page}`).then(response => {
-        this.currentPage = response.data.pagination.currentPage;
-        this.lastPage = response.data.pagination.lastPage;
-        this.words = response.data.words;
-      })
+    el: '#app',
+    data: function () {
+        return {
+            title: 'english dictionary',
+            totalResults: 0,
+            word: '',
+            currentPage: 1,
+            lastPage: -1,
+            words: [],
+            resultsPerPage: 20,
+            pagination: {
+                totalItems: 0,
+                currentPage: 1,
+                pageSize: 1,
+                totalPages: 1,
+                startPage: 1,
+                endPage: 1,
+                startIndex: 0,
+                endIndex: 0,
+                pages: [1, 2]
+            }
+        }
     },
-    search: function () {
-      clearTimeout(debouncer);
-      debouncer = setTimeout(() => {
-        this.query(this.word);
-      }, 666);
+    mounted: function () {
+        this.query('', this.resultsPerPage);
     },
-    goto: function (page) {
-      this.query(this.word, this.resultsPerPage, page);
+    methods: {
+        query: function (word, resultsPerPage, page) {
+            axios.get(`/define?word=${word}&resultsPerPage=${resultsPerPage}&page=${page}`)
+                .then(response => {
+                    this.currentPage = response.data.pagination.currentPage;
+                    this.lastPage = response.data.pagination.lastPage;
+                    this.words = response.data.words;
+                    Object.assign(this.pagination, response.data.pagination);
+                })
+                .then(()=> this.highlight(word));
+        },
+        search: function () {
+            clearTimeout(debouncer);
+            debouncer = setTimeout(() => {
+                this.query(this.word);
+            }, 666);
+        },
+        goto: function (page) {
+            this.query(this.word, this.resultsPerPage, page);
+        },
+        highlight: function (text) {
+            const words = document.querySelectorAll('.word');
+            if(words) {
+                words.forEach(word => {
+                    let wordInnerText = word.innerHTML;
+                    const wrapperStart = `<span style="font-weight: bold;">`;
+                    const wrapperEnd = `</span>`;
+                    wordInnerText = wordInnerText.replace(wrapperStart, '');
+                    wordInnerText = wordInnerText.replace(wrapperEnd, '');
+                    if(wordInnerText.indexOf(text) > -1) {
+                        const highlightedText = `${wrapperStart}${text}${wrapperEnd}`;
+                        const output = wordInnerText.replace(text, highlightedText);
+                        word.innerHTML = output;
+                    } else {
+                        word.innerHTML = wordInnerText;
+                    }
+                });
+            }
+        },
+    },
+    computed: {
+        // pages: function () {
+        //     let range = (a, b) => a > b ? range(b, a).reverse() : (a === b ? [a] : range(a, b - 1).concat(b));
+        //     if (this.currentPage > -1) {
+        //         return range(this.currentPage, (this.lastPage >= this.currentPage + 9) ? this.currentPage + 9 : this.lastPage);
+        //     } else {
+        //         return [];
+        //     }
+        // }
     }
-  },
-  computed: {
-    pages: function () {
-      let range = (a, b) => a > b ? range(b, a).reverse() : (a === b ? [a] : range(a, b - 1).concat(b));
-      if (this.currentPage > -1) {
-        return range(this.currentPage, (this.lastPage >= this.currentPage + 9) ? this.currentPage + 9 : this.lastPage);
-      } else {
-        return [];
-      }
-    }
-  }
 })
