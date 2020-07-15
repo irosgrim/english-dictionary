77a4fb0fbb7486dd71bf9acbadd51239534c9bb1
ion rosgrim <jrunin@gmail.com>
2020-07-15 22:49:06 +0200

    pagination, styling

--- www/app/app.js
+++ www/app/app.js
@@ -6,8 +6,6 @@ const app = new Vue({
             title: 'english dictionary',
             totalResults: 0,
             word: '',
-            currentPage: 1,
-            lastPage: -1,
             words: [],
             resultsPerPage: 20,
             pagination: {
@@ -19,7 +17,7 @@ const app = new Vue({
                 endPage: 1,
                 startIndex: 0,
                 endIndex: 0,
-                pages: [1, 2]
+                pages: [1]
             }
         }
     },
@@ -27,31 +25,33 @@ const app = new Vue({
         this.query('', this.resultsPerPage);
     },
     methods: {
+
         query: function (word, resultsPerPage, page) {
             axios.get(`/define?word=${word}&resultsPerPage=${resultsPerPage}&page=${page}`)
                 .then(response => {
-                    this.currentPage = response.data.pagination.currentPage;
-                    this.lastPage = response.data.pagination.lastPage;
                     this.words = response.data.words;
                     Object.assign(this.pagination, response.data.pagination);
                 })
                 .then(()=> this.highlight(word));
         },
+
         search: function () {
             clearTimeout(debouncer);
             debouncer = setTimeout(() => {
                 this.query(this.word);
-            }, 666);
+            }, 250);
         },
+
         goto: function (page) {
             this.query(this.word, this.resultsPerPage, page);
         },
+
         highlight: function (text) {
             const words = document.querySelectorAll('.word');
             if(words) {
                 words.forEach(word => {
                     let wordInnerText = word.innerHTML;
-                    const wrapperStart = `<span style="font-weight: bold;">`;
+                    const wrapperStart = `<span class="highlight">`;
                     const wrapperEnd = `</span>`;
                     wordInnerText = wordInnerText.replace(wrapperStart, '');
                     wordInnerText = wordInnerText.replace(wrapperEnd, '');
@@ -65,15 +65,6 @@ const app = new Vue({
                 });
             }
         },
-    },
-    computed: {
-        // pages: function () {
-        //     let range = (a, b) => a > b ? range(b, a).reverse() : (a === b ? [a] : range(a, b - 1).concat(b));
-        //     if (this.currentPage > -1) {
-        //         return range(this.currentPage, (this.lastPage >= this.currentPage + 9) ? this.currentPage + 9 : this.lastPage);
-        //     } else {
-        //         return [];
-        //     }
-        // }
+
     }
 })
